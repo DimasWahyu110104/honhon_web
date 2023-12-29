1315e3ccf266d139b19e537f4d50abd5cf8862c8
DimasWahyu110104 <rdimaswahyu07>
2023-12-29 16:16:21 +0700

    menambahkan navbar dan css

--- web_honhon/urls.py
+++ web_honhon/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path("home/", views.home, name="home"),
+    path("menu/", views.menu, name="menu"),
+    path("minuman/", views.minuman, name="minuman"),
+    path("about/", views.about, name="about"),
 
 ]
--- web_honhon/views.py
+++ web_honhon/views.py
@@ -7,3 +7,12 @@ from django.template import loader
 def home(request):
     template = loader.get_template('home.html')
     return HttpResponse(template.render())
+def menu(request):
+    template = loader.get_template('menu.html')
+    return HttpResponse(template.render())
+def minuman(request):
+    template = loader.get_template('minuman.html')
+    return HttpResponse(template.render())
+def about(request):
+    template = loader.get_template('about.html')
+    return HttpResponse(template.render())
